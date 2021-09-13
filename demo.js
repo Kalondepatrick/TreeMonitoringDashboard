2cc7b97ffb6889fdf807d01be41c16e5025b7454
Patrick Ken Kalonde <39599487+Kalondepatrick@users.noreply.github.com>
2021-09-13 04:21:21 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,17 +1,11 @@
-    require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/widgets/Legend", "esri/Color", "esri/renderers/UniqueValueRenderer", "esri/widgets/Slider"], function(esriConfig,Map, MapView, FeatureLayer, Legend, Color, UniqueValueRenderer,Slider) {
+  
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/widgets/Legend", "esri/Color", "esri/renderers/UniqueValueRenderer", "esri/widgets/TimeSlider"], function(esriConfig, Map, MapView, FeatureLayer, Legend, Color, UniqueValueRenderer, TimeSlider) {
       esriConfig.apiKey = MY_API_KEY;  
     
-    var   slider = new Slider({
-        container: "SliderDiv",
-        min:0,
-        max:100, 
-        values: [50],
-        snapOnClickEnabled: false,
-        visibleElemebts: {
-            labels: true,
-            rangeLabels:true
-        }
-    });
+        /********************
+         * Add Basemap and pilot location
+         ********************/
+        
     var map = new Map({
           basemap: "dark-gray-vector"
         });
@@ -25,51 +19,76 @@
           snapToZoom: false
         }
       });
-
-    
+ 
         /********************
-         * Add Feature 
+         * Add Tree layer 
          ********************/
-
-
-
-      // Add a layer on trees 
-      
-      var popuptree ={
-          "title":"Tree Survival",
-          "content": "<b>Status:</b> {survival}"
+ 
+    var popuptree ={
+        "title":"Tree Survival",
+        "content": "<b>Status:</b> {survival}<br><b>Tree Type:</b> {species_ca}"
       }  
-            
       
-		var tree = new FeatureLayer({
-          url: "https://services3.arcgis.com/75NnIQLDaYFUpt2v/arcgis/rest/services/Area22BTreesData/FeatureServer/0",
-          outFields: ["survival"],
-          popupTemplate: popuptree,
+    var tree = new FeatureLayer({
+        url: "https://services3.arcgis.com/75NnIQLDaYFUpt2v/arcgis/rest/services/Area22BTreesData/FeatureServer/0",
+        outFields: ["survival"],
+        popupTemplate: popuptree,
         });
 
 		//Changing the title of tree layer
 
 		tree.title = "Tree survival";
         
-        
-		// Adding the  map to scene 
+		// Adding trees to the map scene 
                 
         map.add(tree, 0);
         
-        var legend = new Legend({
-          view: view
+        tree.when(function (map) {
+          document.getElementById("title").innerHTML =
+            map.portalItem.title + " - Expand Widget";
+        });
+
+        
+        /********************
+         * Add Map Legend 
+         ********************/
+        
+    var legend = new Legend({
+        view: view,
+        style: "classic",
+        layout: "auto"
         });	
 
-       view.ui.add(legend, "top-right")
+       view.ui.add(legend, "bottom-right");
 	   
+         /********************
+         * Adding Map slider 
+         ********************/
+ 
+    
+    const timeSlider = new TimeSlider({
+        container: "SliderDiv",
+        view: view,
+        timevisible: true,
+        fullTimeExtent:{
+            start: new Date(2020,01,29),
+            end: new Date(2021,01,31)
+        },
+        stops:{
+          dates:[ 
+                new Date(2020,01,29),
+                new Date(2020,02,31),
+                new Date(2020,03,30),
+                new Date(2021,01,31)
+            ]
+        }
         
-      
-// Create a simple Slider 
+        });
+    
+        view.ui.add(timeSlider, "bottom-left");
+        
 
-const timeSlider = new TimeSlider({
-  container: "timeSlider",
-  view: view
-})
-view.ui.add(TimeSlider, "bottom-left")
-       
-      });
+  });
+
+ 
+
